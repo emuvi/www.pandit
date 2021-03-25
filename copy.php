cb861855a6b20538cf3766d2c3f106dde0cea536
Everton Murilo Vieira <emuvs@pointel.com.br>
2021-03-25 11:27:59 -0300

    prototype development

---
+++ copy.php
@@ -0,0 +1,6 @@
+<div id="divCopy">
+  <div>Copyright 2021 Pandit.com.br</div>
+  <div>Todos os direitos são reservados.</div>
+  <div>A cópia total ou parcial deste documento <br /> 
+    é permitida desde que seja citada a fonte.</div>
+</div>
---
+++ top.php
@@ -0,0 +1,22 @@
+<div id="divTop" class="no-print">
+  <div id="divLogo">
+    <a href="/index.php" id="menuLogo"></a>
+  </div>
+  <div id="divScope">
+    <a href="/kalpa.php" id="menuKalpa"></a>
+    <a href="/yoga.php" id="menuYoga"></a>
+    <a href="/sunatya.php" id="menuSunatya"></a>
+    <a href="/suyuddha.php" id="menuSuyuddha"></a>
+    <a href="/vatra.php" id="menuVatra"></a>
+    <a href="/okshepa.php" id="menuOkshepa"></a>
+    <a href="/manadi.php" id="menuManadi"></a>
+    <a href="/bhukitsa.php" id="menuBhukitsa"></a>
+  </div>
+  <div id="divAccess">
+    <div id="bodyAccess">
+      <input type="text" placeholder="Usuário" style="width: 120px;" />
+      <input type="password" placeholder="Senha" style="width: 120px;" />
+      <input type="submit" value="Entrar" style="width: 60px;" />
+    </div>
+  </div>
+</div>
